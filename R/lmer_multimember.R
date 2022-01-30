f16efac7fe719e421b05c6e9ed046f5e18a9924e
Jeroen van Paridon <jvparidon@gmail.com>
2022-01-30 01:47:37 -0600

    Cleaned up code and comments a little, annotated some to-do items

--- R/lmer_multimember.R
+++ R/lmer_multimember.R
@@ -1,7 +1,6 @@
 #' @title Multimembership random effects
 #' @description Provides a wrapper around lme4::lmer to allow for multimembership random effects
 #' @name lmer_multimember
-#' @aliases lmer_multimember
 #' @param formula mixed model formula
 #' @param data data frame (possibly but not necessarily containing factors
 #' @param memb_mat list of weights matrices  with which to replace Zt components
@@ -17,48 +16,76 @@
 #' weights <- weights_from_vector(df$memberships)
 #' lmer_multimember(y ~ x + (1|members), df, memb_mat=list(members=weights))
 lmer_multimember <- function(formula, data, memb_mat=list(), ...) {
-  ## FIXME: pass ... through appropriately
+
+  ## FIXME: pass ... through appropriately -> not all args should be passed to same function?
+  # so: use lme4::lmer to figure out which args need to be passed through, and where they go
+  # e.g. control and start go to optimizerLmer()
+  # but devFunOnly needs to be caught in an if/else block
+  # and where does the arg contrasts go? etc. etc.
+
   ## FIXME: test dimensions
+
   # get names of multimembership variables
   mnms <- names(memb_mat)
+
   # get index of bars (location of random effects) in model formula
   fb <- lme4::findbars(formula)
+
   # get random effect grouping variables
   gvars <- vapply(fb, function(x) deparse(x[[3]]), character(1))
+
   Ztlist <- list()
+
+  # iterate over random effects
   for (i in seq_along(fb)) {
+
     fbnm <- deparse(fb[[i]])
+
     ## find corresponding random-effects term
     w <- which(mnms==gvars[i])
-    if (length(w)>0) {
+
+    if (length(w) > 0) {
+
       M <- Matrix::Matrix(memb_mat[[w]])
+
       ## extract LHS (effect)
       form <- as.formula(substitute(~z, list(z=fb[[i]][[2]])))
+
       ## construct model matrix & compute Khatri-Rao product
       X <- model.matrix(form, data=data)
       Zt <- Matrix::KhatriRao(M, t(X), make.dimnames=TRUE)
+
       ## FIXME: mess with names?
       Ztlist[[fbnm]] <- Zt
+
       ## if necessary, add factor to data
       if (!gvars[i] %in% names(data)) {
         ## if the factor has non-trivial ordering, it should be included
         ## in the data.  Do we have to worry about ordering of Z? test!
         data[[gvars[i]]] <- rep_len(factor(rownames(memb_mat[[w]])), dim(data)[1])
       }
-    } ## if  (length(w)>0)
-  } ## for i in seq(fb)
-  ## call lFormula  (FIXME: allow glFormula)
-  lmod <- lme4::lFormula(formula,data=data)
+    }
+  }
+
+  ## call lFormula  (FIXME: allow glFormula) -> is it easiest to have a separate lmerMultiMember::glmer function
+  # that passes all its arguments through to lmerMultiMember::lmer, and then catch that here
+  # and run it through lme4::glFormula instead? (will that work?)
+  lmod <- lme4::lFormula(formula, data=data)
+
   ## substitute new Ztlist elements
   for (m in names(Ztlist)) {
     lmod$reTrms$Ztlist[[m]] <- Ztlist[[m]]
   }
   lmod$reTrms$Zt <- do.call(rbind, lmod$reTrms$Ztlist)
+
   ## finish fitting
   devfun <- do.call(lme4::mkLmerDevfun, lmod)
   opt <- lme4::optimizeLmer(devfun)
   m1 <- lme4::mkMerMod(environment(devfun), opt, lmod$reTrms, fr=lmod$fr)
-  # convert model object to lmerModMultiMember
+
+  # convert model object to lmerModMultiMember -> does this need any additional slots/attributes?
+  # or do we just want to output some additional information when summary.lmerModMultiMember is called?
+  # e.g. for each random effect: don't show number of levels, but number of groups & number of unique group members
   m1 <- as(m1, "lmerModMultiMember")
   return(m1)
 }
@@ -74,6 +101,5 @@ lmer_multimember <- function(formula, data, memb_mat=list(), ...) {
 #' about the multimembership random effects.
 lmerModMultiMember <-
   setClass("lmerModMultiMember",
-           contains = c("lmerMod"),
-           representation = representation(n_levels="numeric")
+           contains = c("lmerMod")
            )
--- R/weights_from_vector.R
+++ R/weights_from_vector.R
@@ -1,7 +1,6 @@
 #' @title Weight matrix from vector of memberships
 #' @description Provides a helper function for generating weight matrices
 #' @name weights_from_vector
-#' @aliases weights_from_vector
 #' @param membership_vector vector of strings containing group memberships
 #' @param sep separator that delimits group memberships in membership_vector
 #' @return sparse weight matrix
@@ -10,15 +9,20 @@
 #' member_vec <-  c("a,b,c", "a,c", "a", "b", "b,a")
 #' weights_from_vector(member_vec)
 weights_from_vector <- function(membership_vector, sep=",") {
+
   # split membership vector into list of vectors
   # (is this necessary? could also just require a list of vectors as input to begin with)
   membership_list <- strsplit(membership_vector, split=sep)
+
   # create list of group membership indices
   idx <- stack(setNames(membership_list, seq_along(membership_list)))
+
   # get sorted vector of unique groups
   groups <- sort(unique(idx$values))
+
   # convert group membership indices to numeric factors
   idx[] <- lapply(idx, function(x) as.numeric(factor(x)))
+
   # initialize sparse weight matrix with zeros
   weights <- Matrix::Matrix(
     0,
@@ -26,8 +30,10 @@ weights_from_vector <- function(membership_vector, sep=",") {
     ncol=length(membership_list),
     dimnames=list(as.character(groups), as.character(seq(length(membership_list))))
   )
+
   # fill in sparse weight matrix with 1s for each group membership
   weights[as.matrix(idx)] <- 1
-  # return transposed sparse weight matrix
+
+  # return sparse weight matrix
   return(weights)
 }
