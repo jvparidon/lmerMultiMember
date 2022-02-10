a2e1501f9b1b6afbe410147ba8f0acd2537806f8
Phillip Alday <me@phillipalday.com>
2022-02-09 23:30:23 -0600

    more tweaks and improvements to shadowing lme4::lmer

--- R/lmer_multimember.R
+++ R/lmer_multimember.R
@@ -7,6 +7,7 @@
 #' @param ... additional arguments to pass through to lme4::lmer
 #' @return lme4 model object
 #' @export
+#' @import lme4
 #' @examples
 #' df <- data.frame(
 #'   x = seq(60) + runif(60, 0, 10),
@@ -15,13 +16,20 @@
 #' )
 #' weights <- weights_from_vector(df$memberships)
 #' lmer(y ~ x + (1|members), df, memberships=list(members=weights))
-lmer <- function(formula, data, memberships=NULL, ...) {
+lmer <- function(formula, data = NULL, REML = TRUE, control = lme4::lmerControl(),
+                 start = NULL, verbose = 0L, weights = NULL, na.action=na.omit,
+                 offset = NULL, contrasts = NULL, devFunOnly = FALSE, memberships=NULL) {
+  ## TODO: call weights_from_vector from within this function
   ## FIXME: docstring
 
   orig_call <- match.call()
   # TODO: detect if lmerTest is loaded
-  if(is.null(memberships)) return(lme4::lmer(formula, data, ...))
-
+  if(is.null(memberships)){
+    return(lme4::lmer(formula, data=data, REML=REML, control=control,
+                      start=start, verbose=verbose, weights=weights,
+                      na.action=na.action, offset=offset, contrasts=contrasts,
+                      devFunOnly=devFunOnly))
+  }
 
   ## FIXME: pass ... through appropriately -> not all args should be passed to same function?
   # so: use lme4::lmer to figure out which args need to be passed through, and where they go
@@ -73,8 +81,9 @@ lmer <- function(formula, data, memberships=NULL, ...) {
     }
   }
 
-
-  lmod <- lme4::lFormula(formula, data=data)
+  lmod <- lFormula(formula, data=data, REML=REML,
+                   weights=weights, na.action=na.action, offset=offset,
+                   contrasts=contrasts, control=control)
 
   ## substitute new Ztlist elements
   for (m in names(Ztlist)) {
@@ -83,12 +92,17 @@ lmer <- function(formula, data, memberships=NULL, ...) {
   lmod$reTrms$Zt <- do.call(rbind, lmod$reTrms$Ztlist)
 
   ## finish fitting
-  devfun <- do.call(lme4::mkLmerDevfun, lmod)
-  opt <- lme4::optimizeLmer(devfun)
-  m1 <- lme4::mkMerMod(environment(devfun), opt, lmod$reTrms, fr=lmod$fr)
+  devfun <- do.call(mkLmerDevfun, lmod)
+  if(devFunOnly) return(devfun)
+  opt <- optimizeLmer(devfun,
+                      optimizer    = control$optimizer,
+                      restart_edge = control$restart_edge,
+                      boundary.tol = control$boundary.tol,
+                      start = NULL,
+                      verbose = 0L,
+                      control = control$optCtrl)
 
-  # this is our baby now
-  m1@call <- orig_call
+  m1 <- mkMerMod(environment(devfun), opt, lmod$reTrms, fr=lmod$fr, mc=orig_call)
 
   # convert model object to lmerModMultiMember -> does this need any additional slots/attributes?
   # or do we just want to output some additional information when summary.lmerModMultiMember is called?
