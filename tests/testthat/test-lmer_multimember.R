9195287e1a5814f15f0da2ab5ea814cb298e6741
Jeroen van Paridon <jvparidon@gmail.com>
2022-04-04 14:49:21 -0500

    Added histogram plotting function for weight matrices, plus unit tests for the weight matrix plotting and histogram functions

--- tests/testthat/test-lmer_multimember.R
+++ tests/testthat/test-lmer_multimember.R
@@ -5,7 +5,7 @@ test_that("lmer return type is both lmerMod and lmerModMultiMember", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  m <- lmerMultiMember::lmer(y ~ x + (1 | members),
+  m <- lmer(y ~ x + (1 | members),
     data = df,
     memberships = list(members = weights)
   )
@@ -20,7 +20,7 @@ test_that("glmer return type is both glmerMod and glmerModMultiMember", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  m <- lmerMultiMember::glmer(y ~ x + (1 | members),
+  m <- glmer(y ~ x + (1 | members),
     data = df,
     family = binomial,
     memberships = list(members = weights)
@@ -36,7 +36,7 @@ test_that("lmer pass through to lme4 works", {
     data = sleepstudy,
     REML = FALSE
   )
-  mm <- lmerMultiMember::lmer(Reaction ~ Days + (1 | Subject),
+  mm <- lmer(Reaction ~ Days + (1 | Subject),
     data = sleepstudy,
     REML = FALSE
   )
@@ -51,7 +51,7 @@ test_that("glmer pass through to lme4 works", {
     data = cbpp,
     family = binomial
   )
-  mm <- lmerMultiMember::glmer(
+  mm <- glmer(
     cbind(incidence, size - incidence) ~ period + (1 | herd),
     data = cbpp,
     family = binomial
@@ -67,7 +67,7 @@ test_that("lmer with devFunOnly = TRUE works", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  m <- lmerMultiMember::lmer(y ~ x + (1 | members),
+  m <- lmer(y ~ x + (1 | members),
     data = df,
     memberships = list(members = weights),
     devFunOnly = TRUE
@@ -82,7 +82,7 @@ test_that("glmer with devFunOnly = TRUE works", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  m <- lmerMultiMember::glmer(y ~ x + (1 | members),
+  m <- glmer(y ~ x + (1 | members),
     data = df,
     family = binomial,
     memberships = list(members = weights),
@@ -98,7 +98,7 @@ test_that("glmer with devFunOnly = TRUE & nAGQ = 0 works", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  m <- lmerMultiMember::glmer(y ~ x + (1 | members),
+  m <- glmer(y ~ x + (1 | members),
     data = df,
     family = binomial,
     memberships = list(members = weights),
@@ -115,7 +115,7 @@ test_that("glmer with start = list(theta = .8) works", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  expect_silent(lmerMultiMember::glmer(y ~ x + (1 | members),
+  expect_silent(glmer(y ~ x + (1 | members),
     data = df,
     family = binomial,
     memberships = list(members = weights),
@@ -130,7 +130,7 @@ test_that("glmer with start = .8 works", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  expect_silent(lmerMultiMember::glmer(y ~ x + (1 | members),
+  expect_silent(glmer(y ~ x + (1 | members),
     data = df,
     family = binomial,
     memberships = list(members = weights),
@@ -145,7 +145,7 @@ test_that("glmer with invalid start parameter throws error", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  expect_error(lmerMultiMember::glmer(y ~ x + (1 | members),
+  expect_error(glmer(y ~ x + (1 | members),
     data = df,
     family = binomial,
     memberships = list(members = weights),
@@ -160,7 +160,7 @@ test_that("glmer with fixef start parameter & nAGQ = 0 throws error", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   weights <- weights_from_vector(df$memberships)
-  expect_error(lmerMultiMember::glmer(y ~ x + (1 | members),
+  expect_error(glmer(y ~ x + (1 | members),
     data = df,
     family = binomial,
     memberships = list(members = weights),
@@ -178,7 +178,7 @@ test_that("summary.merModMultiMember wih incorrect type throws error", {
     memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
   )
   member_matrix <- weights_from_vector(df$memberships)
-  m <- lmerMultiMember::lmer(y ~ x + (1 | members),
+  m <- lmer(y ~ x + (1 | members),
                              data = df,
                              memberships = list(members = member_matrix)
   )
@@ -194,18 +194,58 @@ test_that("summary.merModMultiMember has the right attributes and prints", {
   )
   member_matrix <- weights_from_vector(df$memberships)
   # call with lmer
-  m <- lmerMultiMember::lmer(y ~ x + (1 | members),
+  m <- lmer(y ~ x + (1 | members),
     data = df,
     memberships = list(members = member_matrix)
   )
   expect_equal(member_matrix, summary(m)$memberships$members)
   expect_output(print(summary(m)))
   # call with glmer
-  m <- lmerMultiMember::glmer(y ~ x + (1 | members),
+  m <- glmer(y ~ x + (1 | members),
                               data = df,
                               family = binomial,
                               memberships = list(members = member_matrix)
   )
   expect_equal(member_matrix, summary(m)$memberships$members)
   expect_output(print(summary(m)))
+})
+
+test_that("plot_membership_hist returns a plot for each object type", {
+  df <- data.frame(
+    x = runif(60, 0, 1),
+    y = rbinom(60, 1, 0.6),
+    memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
+  )
+  member_matrix <- weights_from_vector(df$memberships)
+  expect_s3_class(plot_membership_hist(member_matrix),
+                  "histogram")
+  # call with lmer
+  m <- lmer(y ~ x + (1 | members),
+                             data = df,
+                             memberships = list(members = member_matrix)
+  )
+  expect_s3_class(plot_membership_hist(m, "members"),
+                  "histogram")
+  expect_s3_class(plot_membership_hist(summary(m), "members"),
+                  "histogram")
+})
+
+test_that("plot_membership_matrix returns a plot for each object type", {
+  df <- data.frame(
+    x = runif(60, 0, 1),
+    y = rbinom(60, 1, 0.6),
+    memberships = rep(c("a,b,c", "a,c", "a", "b", "b,a", "b,c,a"), 10)
+  )
+  member_matrix <- weights_from_vector(df$memberships)
+  expect_s3_class(plot_membership_matrix(member_matrix),
+                  "trellis")
+  # call with lmer
+  m <- lmer(y ~ x + (1 | members),
+                             data = df,
+                             memberships = list(members = member_matrix)
+  )
+  expect_s3_class(plot_membership_matrix(m, "members"),
+                  "trellis")
+  expect_s3_class(plot_membership_matrix(summary(m), "members"),
+                  "trellis")
 })
